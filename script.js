f90c7ec34738bada1bbbef5387c401d2faf75fe5
Ektamadaan <ektamadaan45@gmail.com>
2024-02-12 21:29:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let student = {
+    name: "John"
+};
+
+// Add a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+// Example usage:
+console.log(student.getKeys()); // Output: ['name']
